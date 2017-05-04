fbc1b3cccd89a3f3f049e49e9acc4f6928f619d5
lorax521 <jraines521@gmail.com>
2017-05-04 16:37:55 -0600

    Update turf.js

--- turf.js
+++ turf.js
@@ -1,8 +1,9 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
 
+/*
 L.geoJson(hospitals).addTo(map);
 L.geoJson(libraries).addTo(map);
-/*
+*/
 for (var i = 0; i < hospitals.features.length; i++) {
   hospitals.features[i].properties['marker-color'] = 'DC143C';
   hospitals.features[i].properties['marker-symbol'] = 'hospital';
@@ -14,13 +15,12 @@ for (var j = 0; j < libraries.features.length; j++) {
   libraries.features[j].properties['marker-symbol'] = 'library';
   libraries.features[j].properties['marker-size'] = 'small';
 }
-*/
+
 var map = L.mapbox.map('map', 'mapbox.light',
   {scrollWheelZoom: true
   }).setView([38.05, -84.5], 12);
-/*
+
 var hospitalLayer = L.mapbox.featureLayer(hospitals).addTo(map);
 var libraryLayer = L.mapbox.featureLayer(libraries).addTo(map);
 
 map.fitBounds(libraryLayer.getBounds());
-*/
