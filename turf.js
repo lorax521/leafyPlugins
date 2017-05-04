c577414a2303d251e9d84645caf87cfe857c9d94
lorax521 <jraines521@gmail.com>
2017-05-04 15:49:14 -0600

    Create turf.js

---
+++ turf.js
@@ -0,0 +1,20 @@
+for (var i = 0; i < hospitals.features.length; i++) {
+  hospitals.features[i].properties['marker-color'] = 'DC143C';
+  hospitals.featires[i].properties[marker-symbol'] = 'hospital';
+  hospitals.features[i].properties['marker-size'] = 'small';
+}
+
+for var j = 0; j < libraries.features.length; j++) {
+  libraries.features[j].properties['marker-color'] = '#4169E1';
+  libraries.features[j].properties['marker-symbol'] = 'library';
+  libraries.features[j].properties['marker-size'] = 'small';
+}
+
+var map = L.mapbox.map('map', 'mapbox.light')
+  .setView([38.05, -84.5], 12);
+map.scrollWheelZoom.disable();
+
+var hospitalLayer = L.mapbox.featureLayer(hospitals).addTo(map);
+var libraryLayer = L.mapbox.featureLayer(libraries).addTo(map);
+
+map.fitBounds(libraryLayer.getBounds());
