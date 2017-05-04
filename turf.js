fe554cd946b30233f50d72c09cf425a649151879
lorax521 <jraines521@gmail.com>
2017-05-04 16:55:39 -0600

    Update turf.js

--- turf.js
+++ turf.js
@@ -1,9 +1,5 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
 
-/*
-L.geoJson(hospitals).addTo(map);
-L.geoJson(libraries).addTo(map);
-*/
 for (var i = 0; i < hospitals.features.length; i++) {
   hospitals.features[i].properties['marker-color'] = 'DC143C';
   hospitals.features[i].properties['marker-symbol'] = 'hospital';
@@ -22,3 +18,21 @@ var hospitalLayer = L.mapbox.featureLayer(hospitals).addTo(map);
 var libraryLayer = L.mapbox.featureLayer(libraries).addTo(map);
 
 map.fitBounds(libraryLayer.getBounds());
+
+//bind a popup to each feature 
+hospitalLayer.eachLayer(function(layer) {
+  layer.bindPopup('<strong>' + layer.feature.properties.Name + '<strong>', {closeButton: false});
+}).addTo(map);
+
+libraryLayer.eachLayer(function(layer) {
+  layer.bindPopup(layer.feature.properties.Name, {closeButton: false});
+}).addTo(map);
+
+//open popups on hover
+libraryLayer.on('mouseover', function(e) {
+  e.layer.openPopup();
+});
+
+hospitialLayer.on('mouseover', function(e) {
+  e.layer.openPopup();
+});
