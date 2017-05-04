b64d6c9dfa8f0a4ad7e9f300cc6e4bc581f617d5
lorax521 <jraines521@gmail.com>
2017-05-04 15:51:26 -0600

    Update turf.js

--- turf.js
+++ turf.js
@@ -1,3 +1,5 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
+
 for (var i = 0; i < hospitals.features.length; i++) {
   hospitals.features[i].properties['marker-color'] = 'DC143C';
   hospitals.featires[i].properties[marker-symbol'] = 'hospital';
