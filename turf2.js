42d9ea72526ac54afa22d6d8f1f24b54ea38f986
lorax521 <jraines521@gmail.com>
2017-05-06 23:21:16 -0600

    Update turf2.js

--- turf2.js
+++ turf2.js
@@ -1,6 +1,18 @@
 L.Mapzen.apiKey = "mapzen-zjC7pba";
 //L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
 
+for (var i = 0; i < hospitals.features.length; i++) {
+  hospitals.features[i].properties['marker-color'] = 'DC143C';
+  hospitals.features[i].properties['marker-symbol'] = 'hospital';
+  hospitals.features[i].properties['marker-size'] = 'small';
+}
+
+for (var j = 0; j < libraries.features.length; j++) {
+  libraries.features[j].properties['marker-color'] = '#4169E1';
+  libraries.features[j].properties['marker-symbol'] = 'library';
+  libraries.features[j].properties['marker-size'] = 'small';
+}
+
 var map = L.map('map').setView([38.05, -84.5], 12);
 
 var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
