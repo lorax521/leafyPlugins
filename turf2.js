591279e7f2b1f50927ddb45a4dea451a0ebadbde
lorax521 <jraines521@gmail.com>
2017-05-10 10:57:16 -0600

    Update turf2.js

--- turf2.js
+++ turf2.js
@@ -1,14 +1,14 @@
 //L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
 //L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2oyZTlneWxzMDdsbzJxbHZ1NHVkY284ciJ9.M99KYDewtwHYmtdJO-j4Eg';
 
-/*
+
 var map = L.map('map').setView([38.05, -84.5], 12);
 
 var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-*/
+
 
 L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d2M1a3N6diJ9.jr45mw3pKka1dCwFfC4aOQ';
 
@@ -26,10 +26,17 @@ L.mapbox.accessToken = 'pk.eyJ1IjoibG9yYXg1MjEiLCJhIjoiY2owaW1uYXBiMDBlZDJxbzM4d
       libraries.features[j].properties['marker-size'] = 'small';
     }
 
+var map = L.map('map').setView([38.05, -84.5], 12);
+
+var OpenStreetMap_BlackAndWhite = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+/*
     var map = L.mapbox.map('map', 'mapbox.light')
       .setView([38.05, -84.5], 12);
     map.scrollWheelZoom.disable();
-
+*/
     var hospitalLayer = L.mapbox.featureLayer(hospitals)
       .addTo(map);
     var libraryLayer = L.mapbox.featureLayer(libraries)
